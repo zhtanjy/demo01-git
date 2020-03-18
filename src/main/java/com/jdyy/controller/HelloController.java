a9d3dd6645d83648ca262932494c1c8fdbbcc594
tan <330524561@qq.com>
2020-03-18 15:54:23 +0800

    第一次提交本地库

---
+++ src/main/java/com/jdyy/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.jdyy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 谭俊彦
+ * @version 1.0 2020-03-18 15:37
+ * @description
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+}
